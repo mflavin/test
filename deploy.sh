52a94be6fb9aae9d5609329bd42c96c83ecf639a
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-13 14:46:41 -0600

    Added... Thu Feb 13 14:46:41 CST 2020

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+git add .
+git commit -m "Added... `date`"
+git push origin master
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/mflavin/test.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 // vue.config.js
 module.exports = {
+    publicPath: '/test/',
     pwa: {
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             swSrc: 'src/service-worker.js',
             exclude: [
-                /\.map$/, 
-                /manifest\.json$/ 
+                /\.map$/,
+                /manifest\.json$/
             ],
         },
         themeColor: '#1da025'
