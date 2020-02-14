b2ed92dab70f035af1cdbc6612dae1ab0b071c5a
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 11:54:19 -0600

    Added... Fri Feb 14 11:54:19 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,52 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-            <h1>first</h1>
-        </div>
-        <button id="refresh-button" v-if="updateExists" @click="refreshApp"><i class="fa fa-refresh"></i> Click to update!</button>
-        <div v-show="showMessage" id="notification">
-            <p>
-                This demo reflects the code from <a href="https://github.com/pimhooghiemstra/plintpwa-vue-1/" target="_blank">this repository</a>. The corresponding post will be available on our blog soon <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>.
-            </p>
-            <button type="button" @click="hideMessage"><i class="fa fa-times"></i></button>
-        </div>
-        <router-view/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/nunu">Nunu</router-link>
+      <h1>WorkBox serviceWorkers...</h1>
+      <h1>Saving</h1>
     </div>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-    export default {
-        data() {
-            return {
-                showMessage: true,
-                blogPost: 'https://www.blog.plint-sites.nl/how-to-add-push-notifications-to-a-progressive-web-app/',
-                // refresh variables
-                refreshing: false,
-                registration: null,
-                updateExists: false,
-            };
-        },
-        methods: {
-            hideMessage() {
-                this.showMessage = false
-            },
-            showRefreshUI(e) {
-                this.registration = e.detail
-                this.updateExists = true
-            },
-            refreshApp() {
-                this.updateExists = false
-                if (!this.registration || !this.registration.waiting) return
-                // send message to SW to skip the waiting and activate the new SW
-                console.log('HOW REFRESHING!!!');
-                this.registration.waiting.postMessage({ type: 'skipWaiting' });
-            },
-        },
-        created() {
-            // ---
-            // Custom code to let user update the app
-            // when a new service worker is available
-            // ---
-            document.addEventListener('swUpdated', this.showRefreshUI, {once: true})
+export default {
+  mounted () {
+    axios
+      .get('https://api.exchangeratesapi.io/latest')
+      .then(response => console.log("Response, ", response.data.rates))
 
-            navigator.serviceWorker.addEventListener('controllerchange', () => {
-                if (this.refreshing) return
-                this.refreshing = true
-                // Here the actual reload of the page occurs
-                window.location.reload()
-            })
-        },
-        mounted () {
-          axios
-            .get('https://api.exchangeratesapi.io/latest')
-            .then(response => console.log("Response, ", response.data.rates))
-
-          axios
-            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => {
-              console.log("Response, ", response.data.bpi);
-              console.log("Response, ", response.data.time.updated)
-            })
-        }
-    };
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        console.log("Response, ", response.data.bpi);
+        console.log("Response, ", response.data.time.updated)
+      })
+  }
+};
 </script>
 
-<style lang="less">
+
+<style>
 #app {
-    font-family: 'Roboto', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-
-button {
-    font-family: 'Permanent Marker', cursive;
-}
-
 #nav {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 900;
-    font-size: 24px;
-    padding: 30px;
-    a {
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
+  padding: 30px;
 }
-#notification {
-    position: relative;
-    width: 280px;
-    font-size: 12px;
-    padding: 0px 24px 24px;;
-    box-sizing: border-box;
-    margin-left: auto;
-    margin-right: auto;
 
-    p {
-        margin: 0;
-    }
-
-    a {
-        color: #42b983;
-    }
-
-    button {
-        position: absolute;
-        top: 0px;
-        right: 0px;
-        padding: 0 5px 2px;
-        background: #1da025;
-        color: #fff;
-        font-size: 13px;
-
-        &:hover {
-            cursor: pointer;
-        }
-    }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-button#refresh-button {
-    width: 240px;
-    background: #a0251d;
-    color: #fff;
-    padding: 10px 20px;
-    font-size: 18px;
-    margin-bottom: 30px;
 
-    &:focus {
-        outline: none;
-    }
-
-    &:disabled {
-        opacity: 0.5;
-        cursor: not-allowed;
-    }
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
