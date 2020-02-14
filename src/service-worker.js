af47134f481b0176148fb402df84a25943443f52
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 10:37:38 -0600

    Added... Fri Feb 14 10:37:38 CST 2020

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,40 +11,28 @@ if (workbox) {
     // Make sure to return a specific response for all navigation requests.
     // Since we have a SPA here, this should be index.html always.
     // https://stackoverflow.com/questions/49963982/vue-router-history-mode-with-pwa-in-offline-mode
-    // workbox.routing.registerNavigationRoute('/index.html')
-
-    workbox.routing.registerNavigationRoute(
-      /^index.html/,
-      new workbox.strategies.NetworkFirst({
-        cacheName: "app",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-          })
-        ],
-      })
-    );
+    workbox.routing.registerNavigationRoute('/index.html')
 
     // Setup cache strategy for Google Fonts. They consist of two parts, a static one
     // coming from fonts.gstatic.com (strategy CacheFirst) and a more ferquently updated on
     // from fonts.googleapis.com. Hence, split in two registerroutes
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.googleapis\.com/,
-        new workbox.strategies.NetworkFirst({
+        new workbox.strategies.StaleWhileRevalidate({
             cacheName: 'google-fonts-stylesheets',
         })
     )
 
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.gstatic\.com/,
-        new workbox.strategies.NetworkFirst({
+        new workbox.strategies.CacheFirst({
             cacheName: 'google-fonts-webfonts',
             plugins: [
                 new workbox.cacheableResponse.Plugin({
                     statuses: [0, 200],
                 }),
                 new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
                     maxEntries: 30,
                 }),
             ],
@@ -53,7 +41,7 @@ if (workbox) {
 
     workbox.routing.registerRoute(
         /^https:\/\/stackpath\.bootstrapcdn\.com/,
-        new workbox.strategies.NetworkFirst({
+        new workbox.strategies.StaleWhileRevalidate({
             cacheName: 'fontawesome',
         })
     );
