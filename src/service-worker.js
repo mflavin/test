b3de5de4184bcc1e761314897d615499f7acb597
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 16:33:15 -0600

    Added... Fri Feb 14 16:33:15 CST 2020

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 // custom service-worker.js
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
