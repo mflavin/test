8f5975e82a3eca9d8e94dcfb9b47cd12a70ebc2a
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 11:52:17 -0600

    Added... Fri Feb 14 11:52:17 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
-            <p>Buffet, Buffo, Buffalo</p>
+            <h1>first</h1>
         </div>
         <button id="refresh-button" v-if="updateExists" @click="refreshApp"><i class="fa fa-refresh"></i> Click to update!</button>
         <div v-show="showMessage" id="notification">
