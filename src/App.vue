266c122940f9bd44624eca912e66bb6779574bfc
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 11:40:34 -0600

    Added... Fri Feb 14 11:40:34 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
-            <h1>this work</h1>
+            <h1>Monka TOS</h1>
         </div>
         <button id="refresh-button" v-if="updateExists" @click="refreshApp"><i class="fa fa-refresh"></i> Click to update!</button>
         <div v-show="showMessage" id="notification">
