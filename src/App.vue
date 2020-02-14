5766652fbb36039813aaf7304df19b52ad148295
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 10:44:17 -0600

    Added... Fri Feb 14 10:44:17 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
             <blockquote cite="http://">
-              plz
+              This is suppose to update>???zzz
             </blockquote>
             <h3>:O <q cite="">The audience</q> Done? :D</h3>
         </div>
