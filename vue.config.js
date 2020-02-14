a1c8758357dc57656d42aef92a29e2128e41cf81
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-14 14:52:37 -0600

    Added... Fri Feb 14 14:52:37 CST 2020

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
-// // vue.config.js
-// module.exports = {
-//     publicPath: '/test/',
-//     pwa: {
-//         workboxPluginMode: 'InjectManifest',
-//         workboxOptions: {
-//             swSrc: 'src/service-worker.js',
-//             exclude: [
-//                 /\.map$/,
-//                 /manifest\.json$/
-//             ],
-//         },
-//         themeColor: '#1da025'
-//     },
-// }
+// vue.config.js
+module.exports = {
+    publicPath: '/test/',
+    pwa: {
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            swSrc: 'src/service-worker.js',
+            exclude: [
+                /\.map$/,
+                /manifest\.json$/
+            ],
+        },
+        themeColor: '#1da025'
+    },
+}
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const WorkboxPlugin = require('workbox-webpack-plugin');
-
-module.exports = {
-  plugins: [
-    new WorkboxPlugin.GenerateSW();
-  ]
-}
