ac33ed6e8c19c121b9701f3ff3eae42db20626c3
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-17 11:22:15 -0600

    Added... Mon Feb 17 11:22:15 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/nunu">Nunu</router-link>
-      <h3>Unga 2</h3>
+      <h3>Round 2</h3>
     </div>
     <router-view/>
   </div>
