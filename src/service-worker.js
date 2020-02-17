4cad995d514b8a71898267b4290793a76ce24233
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-17 11:14:04 -0600

    Added... Mon Feb 17 11:14:04 CST 2020

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,155 +1,67 @@
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-// custom service-worker.js
-// if (workbox) {
-//   console.log(`Yay! Workbox is loaded 🎉`);
-//     // adjust log level for displaying workbox logs
-//     workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
-//
-//     // apply precaching. In the built version, the precacheManifest will
-//     // be imported using importScripts (as is workbox itself) and we can
-//     // precache this. This is all we need for precaching
-//     workbox.precaching.precacheAndRoute(self.__precacheManifest);
-//
-//     // Make sure to return a specific response for all navigation requests.
-//     // Since we have a SPA here, this should be index.html always.
-//     // https://stackoverflow.com/questions/49963982/vue-router-history-mode-with-pwa-in-offline-mode
-//     workbox.routing.registerNavigationRoute('/index.html')
-//
-//     // const ind = '/index.html';
-//     // // Example Data...
-//     // workbox.routing.registerNavigationRoute(
-//     //   ind,
-//     //   new workbox.strategies.NetworkFirst({
-//     //     cacheName: "homepage",
-//     //     plugins: [
-//     //       new workbox.expiration.Plugin({
-//     //         maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-//     //       })
-//     //     ],
-//     //   })
-//     // );
-//
-//     // Setup cache strategy for Google Fonts. They consist of two parts, a static one
-//     // coming from fonts.gstatic.com (strategy CacheFirst) and a more ferquently updated on
-//     // from fonts.googleapis.com. Hence, split in two registerroutes
-//     workbox.routing.registerRoute(
-//         /^https:\/\/fonts\.googleapis\.com/,
-//         new workbox.strategies.StaleWhileRevalidate({
-//             cacheName: 'google-fonts-stylesheets',
-//         })
-//     )
-//
-//     workbox.routing.registerRoute(
-//         /^https:\/\/fonts\.gstatic\.com/,
-//         new workbox.strategies.CacheFirst({
-//             cacheName: 'google-fonts-webfonts',
-//             plugins: [
-//                 new workbox.cacheableResponse.Plugin({
-//                     statuses: [0, 200],
-//                 }),
-//                 new workbox.expiration.Plugin({
-//                     maxAgeSeconds: 60 * 60 * 24 * 365,
-//                     maxEntries: 30,
-//                 }),
-//             ],
-//         })
-//     )
-//
-//     workbox.routing.registerRoute(
-//         /^https:\/\/stackpath\.bootstrapcdn\.com/,
-//         new workbox.strategies.StaleWhileRevalidate({
-//             cacheName: 'fontawesome',
-//         })
-//     );
-//
-//     const test = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-//     // Example Data...
-//     workbox.routing.registerRoute(
-//       test,
-//       new workbox.strategies.NetworkFirst({
-//         cacheName: "currencies",
-//         plugins: [
-//           new workbox.expiration.Plugin({
-//             maxAgeSeconds: 10 * 60 // 10 minutes
-//           })
-//         ],
-//       })
-//     );
-// } else {
-//   console.log(`Boo! Workbox didn't load 😬`);
-// }
-//
-// // This code listens for the user's confirmation to update the app.
-// self.addEventListener('message', (e) => {
-//   console.log('e.data: ', e.data);
-//   console.log('e.data.type: ', e.data.type);
-//     if (e.data.type === 'skipWaiting') {
-//       console.log('skipping...');
-//       self.skipWaiting();
-//     }
-// })
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+// Note: Ignore the error that Glitch raises about workbox being undefined.
+workbox.setConfig({
+  debug: true
+});
 
+// START precaching
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
-  workbox.precaching.precacheAndRoute([]);
+workbox.routing.registerRoute(
+  /((?=([^a-z 0-9]))([^\s])*|)*/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: workbox.core.cacheNames.precache,
+  })
+);
 
-  workbox.routing.registerRoute(
-    /test((?=([^a-z 0-9]))([^\s])*|)*/,
-    workbox.strategies.networkFirst({
-      cacheName: 'subDomain',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        })
-      ]
-    })
-  );
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-  workbox.routing.registerRoute(
-    /http?.:\/\/mflavin\.github\.io\/test((?=([^a-z 0-9]))([^\s])*|)*/,
-    workbox.strategies.networkFirst({
-      cacheName: 'fullURL',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        })
-      ]
-    })
-  );
+workbox.routing.setCatchHandler(({ event }) => {
+  if (isNav(event))
+    return caches.match(workbox.precaching.getCacheKeyForURL('/index.html'));
+  return Response.error();
+});
 
-  // workbox.routing.registerRoute(
-  //   /(.*)about(.*)\.(?:png|gif|jpg)/,
-  //   new workbox.strategies.networkFirst({
-  //     cacheName: 'images-cache',
-  //     plugins: [
-  //       new workbox.expiration.Plugin({
-  //         maxEntries: 50,
-  //         maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-  //       })
-  //     ]
-  //   })
-  // );
-  //
-  // const articleHandler = new workbox.strategies.networkFirst({
-  //   cacheName: 'articles-cache',
-  //   plugins: [
-  //     new workbox.expiration.Plugin({
-  //       maxEntries: 50,
-  //       maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-  //     })
-  //   ]
-  // });
-  //
-  // workbox.routing.registerRoute(/^(.*)about(.*)\.html/, args => {
-  //   return articleHandler.handle(args);
-  // });
+// END precaching
 
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin('myQueueName');
+
+workbox.routing.registerRoute(
+  'https://api.coindesk.com/v1/bpi/currentprice.json',
+  new workbox.strategies.NetworkFirst({
+    plugins: [
+      bgSyncPlugin,
+    ],
+  }),
+);
+workbox.routing.registerRoute(
+  '/index.html',
+  new workbox.strategies.NetworkFirst(),
+);
+
+const {strategies} = workbox;
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  console.log('request: ', request);
+  switch (event.request.url) {
+    case 'https://api.coindesk.com/v1/bpi/currentprice.json':
+      const networkOnly = new workbox.strategies.NetworkFirst();
+      event.respondWith(networkOnly.handle({event, request}));
+      break;
+    case 'https://api.exchangeratesapi.io/latest':
+      const networkFirst = new workbox.strategies.NetworkOnly();
+      event.respondWith(networkFirst.handle({event, request}));
+      break;
+    default:
+      console.log('unable to find URL?');
+      console.log(request);
+      console.log(event);
+  }
+
+});
+
+//This immediately deploys the service worker w/o requiring a refresh
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
