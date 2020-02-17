3506ba8e897e942aca548f879f76615e6538ae4f
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-17 13:12:51 -0600

    Added... Mon Feb 17 13:12:51 CST 2020

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,69 @@
 <template>
     <div class="about">
-        <h1>This is an about page</h1>
+      <body>
+        <header>
+          <ul class="navbar">
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="#">World</a></li>
+            <li><a href="#">Tech</a></li>
+            <li><a href="#">Contact</a></li>
+            <li class="profile-icon"><img src="../images/icon.svg" alt="profile icon" /></li>
+          </ul>
+        </header>
+
+        <article>
+          <h1>New ujpdar</h1>
+          <div class="first">
+          </div>
+          <li><p><button class="fetch">Make fetch request</button></p></li>
+          <li><p><button class="network-only">Network Only Request</button></p></li>
+          <li><p><button class="network-first-valid">Network First Request (Valid URL)</button></p></li>
+          <div class="data">
+          </div>
+          <section>
+            <div class="img-container left column">
+              <img src="../images/business.jpg" alt="A person reading the business section of a newspaper">
+            </div>
+            <div class="right column">
+              <h2 class="headline">Headline</h2>
+              <p class="content">
+                I love cheese, especially manchego swiss. ☺ Fromage queso jarlsberg cheesy
+                feet emmental cottage cheese camembert de normandie bocconcini. Cottage cheese
+                everyone loves cauliflower cheese rubber cheese squirty cheese halloumi cow
+                fondue. Bocconcini cheese and biscuits everyone loves fondue red leicester
+                st. agur blue cheese rubber cheese ricotta. Cheesy grin mozzarella.
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="left column">
+              <h4 class="list-title">Trending Articles</h4>
+              <ul class="vertical-list">
+                <li class="list-item"><a href="pages/article1.html" class="post-link">Article 1</a></li>
+                <li class="list-item"><a href="pages/article2.html" class="post-link">Article 2</a></li>
+                <li class="list-item"><a href="pages/article3.html" class="post-link">Article 3</a></li>
+                <li class="list-item"><a href="pages/article4.html" class="post-link">Article 4</a></li>
+                <li class="list-item"><a href="pages/article-missing.html" class="post-link">Non-existent article</a></li>
+              </ul>
+            </div>
+            <div class="right column">
+              <h4 class="list-title">Archived Posts</h4>
+              <ul class="vertical-list">
+                <li class="list-item"><a href="pages/post1.html" class="post-link">Archived Post 1</a></li>
+                <li class="list-item"><a href="pages/post2.html" class="post-link">Archived Post 2</a></li>
+                <li class="list-item"><a href="pages/post3.html" class="post-link">Archived Post 3</a></li>
+                <li class="list-item"><a href="pages/post-missing.html" class="post-link">Non-existent Post 4</a></li>
+              </ul>
+            </div>
+          </section>
+        </article>
+
+        <footer>
+          <a href="#" class="footer-link">Twitter</a>
+          <a href="#" class="footer-link">Facebook</a>
+          <a href="#" class="footer-link">Google+</a>
+          <a href="#" class="footer-link">Digg</a>
+        </footer>
+      </body>
     </div>
 </template>
---
+++ src/views/article.vue
@@ -0,0 +1,42 @@
+<template>
+  <body>
+
+    <header>
+      <ul class="navbar">
+        <li class="profile-icon"><img src="../images/icon.svg" alt="profile icon" /></li>
+      </ul>
+    </header>
+
+    <article id="post">
+
+      <section>
+
+        <div class="img-container left column">
+          <img src="../images/buildings.jpg" alt="Looking up at skyscrapers">
+        </div>
+
+        <div class="right column">
+          <h2 class="headline">Article 1</h2>
+          <p class="content">
+            I love cheese, especially manchego swiss. ☺ Fromage queso jarlsberg cheesy
+            feet emmental cottage cheese camembert de normandie bocconcini. Cottage cheese
+            everyone loves cauliflower cheese rubber cheese squirty cheese halloumi cow
+            fondue. Bocconcini cheese and biscuits everyone loves fondue red leicester
+            st. agur blue cheese rubber cheese ricotta. Cheesy grin mozzarella.
+          </p>
+        </div>
+
+      </section>
+
+    </article>
+
+    <footer>
+      <a href="#" class="footer-link">Twitter</a>
+      <a href="#" class="footer-link">Facebook</a>
+      <a href="#" class="footer-link">Google+</a>
+      <a href="#" class="footer-link">Digg</a>
+    </footer>
+
+
+  </body>
+</template>
