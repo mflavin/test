834d9c995880bf2f0b2081a38c8bc5d85d1956b6
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-17 12:07:08 -0600

    Added... Mon Feb 17 12:07:08 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <h3>Don't do this to me :( </h3>
+      <router-link to="/about">About</router-link>
+      <small>Done. Just. Done.</small>
     </div>
     <router-view/>
   </div>
