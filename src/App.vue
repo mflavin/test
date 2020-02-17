2de7bdb26c29ef7fd74100c2c54718d068983d50
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-17 13:16:09 -0600

    Added... Mon Feb 17 13:16:09 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/article">Article</router-link>
       <h3 style="font-size: 5px">Done... :D</h3>
       <button type="button" name="button" @click="test">test</button>
     </div>
