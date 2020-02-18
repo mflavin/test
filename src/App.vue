17ac1dd589a8905b23c8ff5753f81d3fedf70244
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-18 07:59:49 -0600

    Added... Tue Feb 18 07:59:49 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/article">Article</router-link>
+      <router-link to="/">... h O m E ...</router-link> |
+      <router-link to="/about">... a B o U t ...</router-link> |
+      <router-link to="/article">... a R t I c L e ...</router-link>
       <small>
         <div class="get"></div>
       </small>
