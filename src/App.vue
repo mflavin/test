56926f774a672761ec4136c8498f497ea050cdd7
Matthew Flavin <mflavin@hawk.iit.edu>
2020-02-18 08:58:09 -0600

    Added... Tue Feb 18 08:58:09 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -143,7 +143,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#app,
+#app {
+  width: 100vw;
+}
+
 body {
   align-items: center;
   display: flex;
