eaa5107c0f1a97b3827c580fa2d5dbc5c0fe57af
mflavin <mflavin@hawk.iit.edu>
2020-09-10 14:37:19 -0500

    Added... Thu Sep 10 14:37:19 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -1,74 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-console.log(`Yay! Workbox is loaded 🎉`);
+console.log('workbox up?');
 
 // Note: Ignore the error that Glitch raises about workbox being undefined.
 workbox.setConfig({
   debug: true
 });
 
-// const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin('myQueueName');
-const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin('queue', {
-    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours
-});
-const broadcastUpdate = new workbox.broadcastUpdate.BroadcastCacheUpdate("broadcast-update-demo");
-
-// // Adding everything to cache
-// workbox.routing.registerRoute(
-//   /((?=([^a-z 0-9]))([^\s])*|)*/,
-//   new workbox.strategies.NetworkFirst({
-//     cacheName: workbox.core.cacheNames.precache,
-//     plugins: [
-//       broadcastUpdate,
-//     ],
-//   })
-// );
-
 // Precaching to allow for offline
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
-// workbox.precaching.precacheAndRoute(self.__precacheManifest,
-//   new workbox.strategies.NetworkOnly({
-//     plugins: [
-//       bgSyncPlugin,
-//       new workbox.expiration.CacheExpiration({
-//         maxAgeSeconds: 10, // 1 year
-//       })
-//     ],
-//   }),
-// );
-
-// Looks for network connect for data, if none, uses cached data
-workbox.routing.registerRoute(
-  'https://api.exchangeratesapi.io/latest',
-  new workbox.strategies.NetworkFirst(),
-);
-
-// Looks for network connect for data, if none, uses cached data
-workbox.routing.registerRoute(
-  'https://api.coindesk.com/v1/bpi/currentprice.json',
-  new workbox.strategies.NetworkFirst(),
-);
-
-// TODO: Future function. Used to finish REST requests once connection is remade
-// This is running, turn off wifi, click button, watch network tab, turn on button
-// Watch requests come in with new internet connection
-
-// GET
-workbox.routing.registerRoute(
-  'https://api.coindesk.com/v1/bpi/currentprice/CNY.json',
-  new workbox.strategies.NetworkFirst({
-    plugins: [bgSyncPlugin],
-  }),
-);
-// POST
-workbox.routing.registerRoute(
-  'https://jsonplaceholder.typicode.com/posts',
-  new workbox.strategies.NetworkFirst({
-    plugins: [bgSyncPlugin],
-  }),
-  'POST'
-);
 
 //This immediately deploys the service worker w/o requiring a refresh
 workbox.core.skipWaiting();
