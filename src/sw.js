794861c4eff455fcab9f58f2af5bbbbf61a050df
mflavin <mflavin@hawk.iit.edu>
2020-09-10 14:33:58 -0500

    Added... Thu Sep 10 14:33:58 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
