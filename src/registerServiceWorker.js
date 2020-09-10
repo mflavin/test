aac831341da77b130444f2fe530f69f5b486524b
mflavin <mflavin@hawk.iit.edu>
2020-09-10 14:53:39 -0500

    Added... Thu Sep 10 14:53:39 CDT 2020

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -22,7 +22,7 @@ fetch("https://api.exchangeratesapi.io/latest")
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    const wb = new Workbox("/sw.js");
+    const wb = new Workbox(`${process.env.BASE_URL}sw.js`);
     const updateButton = document.querySelector("#app-update");
     // Fires when the registered service worker has installed but is waiting to activate.
     wb.addEventListener("waiting", event => {
