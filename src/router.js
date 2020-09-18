a61c6d9bb32d02a520f9dfe33d056e774198d35e
mflavin <mflavin@hawk.iit.edu>
2020-09-18 16:19:27 -0500

    Added... Fri Sep 18 16:19:26 CDT 2020

--- src/router.js
+++ src/router.js
@@ -31,6 +31,10 @@ export default new Router({
             './views/About.vue'
           )
         },
+        {
+          path: '*',
+          redirect: '/',
+        }
       ],
     },
     {
