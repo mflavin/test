81f7e3d418c116653da5b5f7fa64f12dcb6702fb
mflavin <mflavin@hawk.iit.edu>
2020-09-18 16:05:34 -0500

    Added... Fri Sep 18 16:05:34 CDT 2020

--- src/router.js
+++ src/router.js
@@ -19,6 +19,19 @@ export default new Router({
       component: Home
     },
     {
+      path: 'test/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(
+        /* webpackMode: "lazy" */
+        /* webpackPrefetch: true */
+        /* webpackChunkName: "about" */
+        './views/About.vue'
+      )
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
