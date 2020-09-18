7c9354daa09990501c14c83c090e5fc216757220
mflavin <mflavin@hawk.iit.edu>
2020-09-18 14:26:10 -0500

    Added... Fri Sep 18 14:26:10 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -2,14 +2,18 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox
 
 // Note: Ignore the error that Glitch raises about workbox being undefined.
 workbox.setConfig({
-  debug: true
+  debug: true,
 });
 
-workbox.routing.registerRoute(new RegExp('/.*'), new workbox.strategies.NetworkFirst());
-
 // Precaching to allow for offline
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-//This immediately deploys the service worker w/o requiring a refresh
+// 'With this in place, the '/{NESTED_ROUTE_NAME}' route also works offline.'
+// SOURCE: https://www.blog.plint-sites.nl/adding-workbox-to-a-vue-cli-pwa/
+workbox.routing.registerRoute('/index.html');
+workbox.routing.registerRoute('/about/');
+
+// This immediately deploys the service worker w/o requiring a refresh
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
