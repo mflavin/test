0bc3ecf53461d505e11f668569553eba729ba674
mflavin <mflavin@hawk.iit.edu>
2020-10-15 12:01:13 -0500

    Added... Thu Oct 15 12:01:13 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -86,7 +86,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   ({ event }) => event.request.mode === 'navigate',
   async () => {
-    const defaultBase = '/index.html';
+    const defaultBase = './views/offline.vue';
     console.log('defaultBase, ', defaultBase);
     return caches
       .match(workbox.precaching.getCacheKeyForURL(defaultBase))
