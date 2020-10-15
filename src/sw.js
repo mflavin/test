34a1e5dae20b503381360ba1e2750b92d737c711
mflavin <mflavin@hawk.iit.edu>
2020-10-15 11:33:08 -0500

    Added... Thu Oct 15 11:33:08 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -113,14 +113,6 @@ const CACHE_NAME = 'offline-html';
 // This assumes /offline.html is a URL for your self-contained
 // (no external images or styles) offline page.
 const FALLBACK_HTML_URL = '/offline.html';
-// Populate the cache with the offline HTML page when the
-// service worker is installed.
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.add(FALLBACK_HTML_URL))
-  );
-});
 
 const networkOnly = new workbox.strategies.NetworkOnly();
 const navigationHandler = async (params) => {
