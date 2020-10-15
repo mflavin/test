ab849e5a6ba321e10eca712c3def8b57fc7fd84d
mflavin <mflavin@hawk.iit.edu>
2020-10-15 11:24:58 -0500

    Added... Thu Oct 15 11:24:58 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -92,3 +92,18 @@ self.addEventListener('install', (event) => {
   const assetsLoaded = fetch(preInstallUrl);
   event.waitUntil(assetsLoaded);
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match(workbox.precaching.getCacheKeyForURL('/offline.html'));
+          // workbox.precaching.precacheAndRoute
+        }
+      });
+    })
+  );
+});
