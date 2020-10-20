931895ec98875b045e3a19c03c6886365be6a689
mflavin <mflavin@hawk.iit.edu>
2020-10-20 16:58:17 -0500

    Added... Tue Oct 20 16:58:17 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -120,6 +120,9 @@
 // ====== TESTING BELOW ======
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+// Precaching to allow for offline
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
 //This immediately deploys the service worker w/o requiring a refresh
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
