8eb5427d6f90c8f6725ab153707e9610a7a44824
mflavin <mflavin@hawk.iit.edu>
2020-10-21 09:36:12 -0500

    Added... Wed Oct 21 09:36:12 CDT 2020

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,4 +1,3 @@
-import { Workbox } from 'workbox-window';
 /**
  * Copyright 2015 Google Inc. All rights reserved.
  *
@@ -19,78 +18,84 @@ import { Workbox } from 'workbox-window';
 /* eslint-disable-next-line */
 'use strict';
 
-// if ('serviceWorker' in navigator) {
-//   // Delay registration until after the page has loaded, to ensure that our
-//   // precaching requests don't degrade the first visit experience.
-//   // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-//   window.addEventListener('load', () => {
-//     // Your service-worker.js *must* be located at the top-level directory relative to your site.
-//     // It won't be able to control pages unless it's located at the same level or higher than them.
-//     // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-//     // See https://github.com/slightlyoff/ServiceWorker/issues/468
-//     navigator.serviceWorker.register(`${process.env.BASE_URL}sw.js`).then((reg) => {
-//       const app = document.getElementById('app').__vue__;
-//       // Watches to see if route changed then checks for new code
-//       // TODO: Test this a bit more, make sure it doesn't mess up with form submits or similar page actions
-//       app.$watch('$route.path', () => {
-//         console.log('Update...');
-//         reg.update();
-//       });
-//       // updatefound is fired if service-worker.js changes.
-//       /* eslint-disable-next-line */
-//       reg.onupdatefound = () => {
-//         // The updatefound event implies that reg.installing is set; see
-//         // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-//         const installingWorker = reg.installing;
-//
-//         /* eslint-disable-next-line */
-//         installingWorker.onstatechange = () => {
-//           switch (installingWorker.state) {
-//             case 'installed':
-//               if (navigator.serviceWorker.controller) {
-//                 // At this point, the old content will have been purged and the fresh content will
-//                 // have been added to the cache.
-//                 // It's the perfect time to display a "New content is available; please refresh."
-//                 // message in the page's interface.
-//                 console.log('New content is available; please refresh.');
-//                 // New content, auto refresh
-//                 reg.unregister().then(() => {
-//                   console.log('Reload serviceWorker...');
-//                   window.location.reload(true);
-//                 });
-//               } else {
-//                 // At this point, everything has been precached.
-//                 // It's the perfect time to display a "Content is cached for offline use." message.
-//                 console.log('Content is now available offline!');
-//               }
-//               break;
-//
-//             case 'redundant':
-//               reg.unregister().then(() => {
-//                 console.error('The installing service worker became redundant.');
-//                 window.location.reload(true);
-//               });
-//               break;
-//
-//             default:
-//               console.log('Default Switch Case.');
-//           }
-//         };
-//       };
-//     }).catch((e) => {
-//       console.error('Error during service worker registration:', e);
-//     });
-//     let refreshing;
-//     navigator.serviceWorker.addEventListener('controllerchange', () => {
-//       if (refreshing) return;
-//       window.location.reload();
-//       refreshing = true;
-//     });
-//   });
-// }
-
 if ('serviceWorker' in navigator) {
-  const wb = new Workbox(`${process.env.BASE_URL}sw.js`);
-  wb.messageSW({ process: process });
-  wb.register();
+  // Delay registration until after the page has loaded, to ensure that our
+  // precaching requests don't degrade the first visit experience.
+  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
+  window.addEventListener('load', () => {
+    // Your service-worker.js *must* be located at the top-level directory relative to your site.
+    // It won't be able to control pages unless it's located at the same level or higher than them.
+    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
+    // See https://github.com/slightlyoff/ServiceWorker/issues/468
+    navigator.serviceWorker.register(`${process.env.BASE_URL}sw.js`).then((reg) => {
+      const app = document.getElementById('app').__vue__;
+      // Watches to see if route changed then checks for new code
+      // TODO: Test this a bit more, make sure it doesn't mess up with form submits or similar page actions
+      app.$watch('$route.path', () => {
+        console.log('Update...');
+        reg.update();
+      });
+      // updatefound is fired if service-worker.js changes.
+      /* eslint-disable-next-line */
+      reg.onupdatefound = () => {
+        // The updatefound event implies that reg.installing is set; see
+        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
+        const installingWorker = reg.installing;
+
+        /* eslint-disable-next-line */
+        installingWorker.onstatechange = () => {
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                // At this point, the old content will have been purged and the fresh content will
+                // have been added to the cache.
+                // It's the perfect time to display a "New content is available; please refresh."
+                // message in the page's interface.
+                console.log('New content is available; please refresh.');
+                // New content, auto refresh
+                reg.unregister().then(() => {
+                  console.log('Reload serviceWorker...');
+                  window.location.reload(true);
+                });
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a "Content is cached for offline use." message.
+                console.log('Content is now available offline!');
+              }
+              break;
+
+            case 'redundant':
+              reg.unregister().then(() => {
+                console.error('The installing service worker became redundant.');
+                window.location.reload(true);
+              });
+              break;
+
+            default:
+              console.log('Default Switch Case.');
+          }
+        };
+      };
+    }).catch((e) => {
+      console.error('Error during service worker registration:', e);
+    });
+
+    let refreshing;
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (refreshing) return;
+      window.location.reload();
+      refreshing = true;
+    });
+
+    navigator.serviceWorker.addEventListener('message', event => {
+      // event is a MessageEvent object
+      console.log(`The service worker sent me a message: ${event.data}`);
+    });
+
+    navigator.serviceWorker.ready.then(registration => {
+      registration.active.postMessage('Hi service worker');
+    });
+
+  });
 }
