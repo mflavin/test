c4486f99a93b5f6bc95a2e56cc6ecf96cca9db19
mflavin <mflavin@hawk.iit.edu>
2020-10-21 17:15:25 -0500

    Added... Wed Oct 21 17:15:25 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -16,14 +16,14 @@ Copyright 2015, 2019 Google Inc. All Rights Reserved.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
-const OFFLINE_URL = './offline.html';
+const OFFLINE_URL = 'offline.html';
+const OFFLINE_URL_TWO = 'public/offline.html';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+    await cache.add(new Request(OFFLINE_URL));
+    await cache.add(new Request(OFFLINE_URL_TWO));
   })());
 });
 
