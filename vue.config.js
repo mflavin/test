eeb25285988ccbbb9b65702436e7287ea1204069
mflavin <mflavin@hawk.iit.edu>
2020-10-21 17:23:01 -0500

    Added... Wed Oct 21 17:23:01 CDT 2020

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 // NOTE: True when testing, should be false when not in dev or not needed
 const enableAnalyze = false;
@@ -25,19 +26,40 @@ module.exports = {
           }),
         ]
         : [],
+        // service worker caching
+        new SWPrecacheWebpackPlugin({
+          cacheId: 'my-vue-app',
+          filename: 'service-worker.js',
+          staticFileGlobs: ['dist/**/*.{js,html,css}'],
+          minify: true,
+          stripPrefix: 'dist/',
+          runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+            handler: 'cacheFirst'
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+            handler: 'cacheFirst'
+          },
+          {
+            urlPattern: /^https:\/\/code\.getmdl\.io\//,
+            handler: 'cacheFirst'
+          }]
+        })
     ],
     // if you don't put the "/" here, you get this error:
     // "bundle.js:1 Uncaught SyntaxError: Unexpected token <"
   },
   publicPath: '/test/',
   // Used to configure serviceWorker
-  pwa: {
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/sw.js',
-      // ...other Workbox options...
-    },
-  },
+  // pwa: {
+  //   // configure the workbox plugin
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     // swSrc is required in InjectManifest mode.
+  //     swSrc: 'src/sw.js',
+  //     // ...other Workbox options...
+  //   },
+  // },
 };
