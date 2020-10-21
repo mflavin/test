8abe197d0948bb05abf7bc23bb6c7817e4c5c9c2
mflavin <mflavin@hawk.iit.edu>
2020-10-21 09:29:44 -0500

    Added... Wed Oct 21 09:29:44 CDT 2020

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,4 +1,4 @@
-import workbox from 'https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js';
+import { Workbox } from 'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-window.prod.mjs';
 /**
  * Copyright 2015 Google Inc. All rights reserved.
  *
