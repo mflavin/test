98638576c85ae3f33cb7933540bdddd8b389f13a
mflavin <mflavin@hawk.iit.edu>
2020-10-21 13:10:45 -0500

    Added... Wed Oct 21 13:10:45 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -106,24 +106,24 @@ workbox.routing.registerRoute(
   }
 );
 
-const cacheOnly = new workbox.strategies.CacheOnly();
-const networkFirst = new workbox.strategies.NetworkFirst();
-
-workbox.routing.registerRoute(
-  /\/test\/.*/,
-  async args => {
-    const offlineRequest = new Request('/test/offline');
-    console.log('offlineRequest, ', offlineRequest);
-    try {
-      const response = await networkFirst.handle(args);
-      console.log('response || await cacheOnly.handle({request: offlineRequest})');
-      console.log(response || await cacheOnly.handle({request: offlineRequest}));
-      return response || await cacheOnly.handle({request: offlineRequest});
-    } catch (error) {
-      return await cacheOnly.handle({request: offlineRequest})
-    }
-  }
-);
+// const cacheOnly = new workbox.strategies.CacheOnly();
+// const networkFirst = new workbox.strategies.NetworkFirst();
+//
+// workbox.routing.registerRoute(
+//   /\/test\/.*/,
+//   async args => {
+//     const offlineRequest = new Request('/test/offline');
+//     console.log('offlineRequest, ', offlineRequest);
+//     try {
+//       const response = await networkFirst.handle(args);
+//       console.log('response || await cacheOnly.handle({request: offlineRequest})');
+//       console.log(response || await cacheOnly.handle({request: offlineRequest}));
+//       return response || await cacheOnly.handle({request: offlineRequest});
+//     } catch (error) {
+//       return await cacheOnly.handle({request: offlineRequest})
+//     }
+//   }
+// );
 
 self.addEventListener('message', (event) => {
   console.log('event, ', event);
