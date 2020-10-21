92ffcace74a9b76ea62a8eecb4b0080ae91ae5d9
mflavin <mflavin@hawk.iit.edu>
2020-10-21 10:33:46 -0500

    Added... Wed Oct 21 10:33:46 CDT 2020

--- src/router.js
+++ src/router.js
@@ -50,8 +50,8 @@ export default new Router({
       ),
       children: [
         {
-          path: '/article',
-          name: 'Article',
+          path: '/cheese',
+          name: 'cheese',
           // route level code-splitting
           // this generates a separate chunk (about.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
