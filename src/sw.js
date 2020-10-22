ffa7e2f85c1732bcef8a19079ce683d6c121c70a
mflavin <mflavin@hawk.iit.edu>
2020-10-22 13:54:54 -0500

    Added... Thu Oct 22 13:54:54 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -35,6 +35,23 @@ workbox.routing.registerRoute(
   }
 );
 
+self.addEventListener('fetch',
+  ({ event }) => event.request.mode === 'navigate',
+  async () => {
+    // const defaultBase = globalRoute || '/';
+    const defaultBase = '/test/';
+    console.log('globalRoute, ', globalRoute);
+    return caches
+      .match(workbox.precaching.getCacheKeyForURL(defaultBase))
+      .then(response => {
+        return response || fetch(defaultBase);
+      })
+      .catch(err => {
+        return fetch(defaultBase);
+      });
+  }
+);
+
 // This immediately deploys the service worker w/o requiring a refresh
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
