29454b671b46f115d1cbff934cd0a6efbdf326fb
mflavin <mflavin@hawk.iit.edu>
2020-10-23 14:20:37 -0500

    Added... Fri Oct 23 14:20:37 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -18,22 +18,22 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 // default page handler for offline usage,
 // where the browser does not how to handle deep links
 // it's a SPA, so each path that is a navigation should default to index.html
-workbox.routing.registerRoute(
-  ({ event }) => event.request.mode === 'navigate',
-  async () => {
-    // const defaultBase = globalRoute || '/';
-    const defaultBase = '/test/';
-    console.log('globalRoute, ', globalRoute);
-    return caches
-      .match(workbox.precaching.getCacheKeyForURL(defaultBase))
-      .then(response => {
-        return response || fetch(defaultBase);
-      })
-      .catch(err => {
-        return fetch(defaultBase);
-      });
-  }
-);
+// workbox.routing.registerRoute(
+//   ({ event }) => event.request.mode === 'navigate',
+//   async () => {
+//     // const defaultBase = globalRoute || '/';
+//     const defaultBase = '/test/';
+//     console.log('globalRoute, ', globalRoute);
+//     return caches
+//       .match(workbox.precaching.getCacheKeyForURL(defaultBase))
+//       .then(response => {
+//         return response || fetch(defaultBase);
+//       })
+//       .catch(err => {
+//         return fetch(defaultBase);
+//       });
+//   }
+// );
 
 self.addEventListener('fetch', function(event) {
   const defaultBase = '/test/';
