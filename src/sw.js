b39e07958810fe5c274ef5f528adb66cde29c45f
mflavin <mflavin@hawk.iit.edu>
2020-10-23 14:22:04 -0500

    Added... Fri Oct 23 14:22:04 CDT 2020

--- src/sw.js
+++ src/sw.js
@@ -18,46 +18,37 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 // default page handler for offline usage,
 // where the browser does not how to handle deep links
 // it's a SPA, so each path that is a navigation should default to index.html
-// workbox.routing.registerRoute(
-//   ({ event }) => event.request.mode === 'navigate',
-//   async () => {
-//     // const defaultBase = globalRoute || '/';
-//     const defaultBase = '/test/';
-//     console.log('globalRoute, ', globalRoute);
-//     return caches
-//       .match(workbox.precaching.getCacheKeyForURL(defaultBase))
-//       .then(response => {
-//         return response || fetch(defaultBase);
-//       })
-//       .catch(err => {
-//         return fetch(defaultBase);
-//       });
-//   }
-// );
-
-self.addEventListener('fetch', function(event) {
-  const defaultBase = '/test/';
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches
-                .match(workbox.precaching.getCacheKeyForURL(defaultBase))
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return fetch('/offline.html');
-            });
-        }
+workbox.routing.registerRoute(
+  ({ event }) => event.request.mode === 'navigate',
+  async () => {
+    // const defaultBase = globalRoute || '/';
+    const defaultBase = '/test/';
+    console.log('globalRoute, ', globalRoute);
+    return caches
+      .match(workbox.precaching.getCacheKeyForURL(defaultBase))
+      .then(response => {
+        return response || fetch(defaultBase);
       })
-  );
+      .catch(err => {
+        return fetch(defaultBase);
+      });
+  }
+);
+
+self.addEventListener('fetch', async event => {
+  if (event.request.mode === 'navigate') {
+    // const defaultBase = globalRoute || '/';
+    const defaultBase = '/test/';
+    console.log('globalRoute, ', globalRoute);
+    return caches
+      .match(workbox.precaching.getCacheKeyForURL(defaultBase))
+      .then(response => {
+        return response || fetch(defaultBase);
+      })
+      .catch(err => {
+        return fetch(defaultBase);
+      });
+  }
 });
 
 // This immediately deploys the service worker w/o requiring a refresh
