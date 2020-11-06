1420b313d6089456a2e946e82cb3b1738c92812e
mflavin <mflavin@hawk.iit.edu>
2020-11-06 11:49:32 -0600

    Added... Fri Nov  6 11:49:32 CST 2020

--- src/sw.js
+++ src/sw.js
@@ -32,9 +32,8 @@ workbox.routing.registerRoute(
 // https://medium.com/@jono/cache-graphql-post-requests-with-service-worker-100a822a388a
 workbox.routing.registerRoute(
   'https://graphqlzero.almansi.me/api',
-  new workbox.strategies.StaleWhileRevalidate()
-  // new workbox.strategies.StaleWhileRevalidate(),
-  // 'POST'
+  new workbox.strategies.StaleWhileRevalidate(),
+  'POST'
 );
 
 // default page handler for offline usage,
