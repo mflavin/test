46fb33db2ecacded6d0e6ea26bdbe3e55435a22c
mflavin <mflavin@hawk.iit.edu>
2020-11-06 10:50:48 -0600

    Added... Fri Nov  6 10:50:48 CST 2020

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,7 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/md5.js');
+importScripts('https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js');
 
 let globalRoute;
 
