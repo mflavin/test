70b6e5601af9f013dde87fd02756a9f66b4de056
mflavin <mflavin@hawk.iit.edu>
2020-11-06 11:01:02 -0600

    Added... Fri Nov  6 11:01:02 CST 2020

--- src/sw.js
+++ src/sw.js
@@ -29,20 +29,20 @@ workbox.routing.registerRoute(
   new workbox.strategies.StaleWhileRevalidate(),
 );
 
-workbox.routing.registerRoute(
-  'https://graphqlzero.almansi.me/api',
-  new workbox.strategies.StaleWhileRevalidate(),
-);
+// workbox.routing.registerRoute(
+//   'https://graphqlzero.almansi.me/api',
+//   new workbox.strategies.StaleWhileRevalidate(),
+// );
 
 // https://medium.com/@jono/cache-graphql-post-requests-with-service-worker-100a822a388a
 workbox.routing.registerRoute(
   '/api',
-  async ({event}) => new workbox.strategies.StaleWhileRevalidate(event),
+  new workbox.strategies.StaleWhileRevalidate(),
   'POST'
 );
 workbox.routing.registerRoute(
   'https://graphqlzero.almansi.me/api',
-  async ({event}) => new workbox.strategies.StaleWhileRevalidate(event),
+  new workbox.strategies.StaleWhileRevalidate(),
   'POST'
 );
 
