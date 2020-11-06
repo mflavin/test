dc7b9652a1ae8f97315b8b803a851ae4e078f4f7
mflavin <mflavin@hawk.iit.edu>
2020-11-06 15:07:19 -0600

    Added... Fri Nov  6 15:07:19 CST 2020

---
+++ src/gqlQuery.js
@@ -0,0 +1,185 @@
+export const shortQuery = `{
+  cities {
+    createdAt
+    id
+    name
+    type
+    updatedAt
+    country {
+      cities {
+        createdAt
+        id
+        name
+        type
+        updatedAt
+      }
+      createdAt
+      id
+      isoCode
+      name
+      type
+      updatedAt
+      jobs {
+        applyUrl
+        commitment {
+          id
+          createdAt
+          title
+          updatedAt
+        }
+        company {
+          createdAt
+          emailed
+          id
+          logoUrl
+          name
+          twitter
+          updatedAt
+          websiteUrl
+        }
+        description
+        id
+        isFeatured
+        isPublished
+        locationNames
+        postedAt
+        title
+        updatedAt
+        userEmail
+      }
+    }
+    jobs {
+      applyUrl
+      createdAt
+      description
+      id
+      isFeatured
+      isPublished
+      locationNames
+      postedAt
+      title
+      updatedAt
+      userEmail
+      tags {
+        updatedAt
+        name
+        id
+        createdAt
+      }
+      remotes {
+        type
+        updatedAt
+        name
+        id
+        createdAt
+      }
+      countries {
+        updatedAt
+        type
+        name
+        isoCode
+        createdAt
+        id
+        cities {
+          updatedAt
+          type
+          name
+          id
+          createdAt
+          country {
+            updatedAt
+            type
+            name
+            isoCode
+            id
+            createdAt
+            cities {
+              createdAt
+              id
+              name
+              slug
+              updatedAt
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+export const longQuery = `{
+  cities {
+    country {
+      updatedAt
+      createdAt
+      id
+      type
+      name
+      jobs {
+        isFeatured
+        isPublished
+        locationNames
+        postedAt
+        title
+        updatedAt
+        userEmail
+        id
+        description
+        createdAt
+        company {
+          createdAt
+          emailed
+          id
+          logoUrl
+          name
+          twitter
+          updatedAt
+          websiteUrl
+        }
+        commitment {
+          id
+          createdAt
+          title
+          updatedAt
+        }
+        applyUrl
+        countries {
+          jobs {
+            isFeatured
+            isPublished
+            locationNames
+            postedAt
+            title
+            updatedAt
+            userEmail
+            id
+            description
+            createdAt
+            commitment {
+              id
+              createdAt
+              title
+              updatedAt
+            }
+            applyUrl
+            countries {
+              jobs {
+                isFeatured
+                isPublished
+                locationNames
+                postedAt
+                title
+                updatedAt
+                userEmail
+                id
+                description
+                createdAt
+              }
+            }
+    			}
+        }
+      }
+    }
+  }
+}`;
