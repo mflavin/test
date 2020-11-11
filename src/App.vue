b65807d3994b8340e021ed18f671ba1fd3591efb
mflavin <mflavin@hawk.iit.edu>
2020-11-11 15:36:51 -0600

    Added... Wed Nov 11 15:36:51 CST 2020

--- src/App.vue
+++ src/App.vue
@@ -221,12 +221,13 @@ export default {
       "mode": "cors",
       "credentials": "omit"
     })
-    .then((response) => {
-      console.log('dev-forms.myqsrsoft -- ', response);
-      response.text().then(d => {
-        console.log('d: ', d);
-        console.log('JSON.parse(d) ', JSON.parse(d));
-      })
+    .then(function(response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.json();
+    }).then(function(data) {
+      // `data` is the parsed version of the JSON returned from the above endpoint.
+      console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
     });
   }
 };
