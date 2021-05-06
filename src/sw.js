a4739afb58e3957b37ad78e74f5ec6bcfb304b46
mflavin <mflavin@hawk.iit.edu>
2021-05-06 11:03:46 -0500

    Added... Thu, May  6, 2021 11:03:46 AM

--- src/sw.js
+++ src/sw.js
@@ -1,82 +1,72 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
 
+// workbox.routing.registerRoute(
+// 	({
+// 		request
+// 	}) => request.destination === 'script' ||
+// 	request.destination === 'style',
+// 	new workbox.strategies.StaleWhileRevalidate()
+// );
+
+
+
+
+
+// Cache page navigations (html) with a Network First strategy
 workbox.routing.registerRoute(
-	({
-		request
-	}) => request.destination === 'script' ||
-	request.destination === 'style',
-	new workbox.strategies.StaleWhileRevalidate()
+    // Check to see if the request is a navigation to a new page
+    ({ request }) => request.mode === 'navigate',
+    // Use a Network First caching strategy
+    new workbox.strategies.NetworkFirst({
+        // Put all cached files in a cache named 'pages'
+        cacheName: 'pages',
+        plugins: [
+            // Ensure that only requests that result in a 200 status are cached
+            new workbox.cacheableResponse.Plugin({
+                statuses: [200],
+            }),
+        ],
+    }),
 );
 
+// Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
+workbox.routing.registerRoute(
+    // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
+    ({ request }) =>
+        request.destination === 'style' ||
+        request.destination === 'script' ||
+        request.destination === 'worker',
+    // Use a Stale While Revalidate caching strategy
+    new workbox.strategies.StaleWhileRevalidate({
+        // Put all cached files in a cache named 'assets'
+        cacheName: 'assets',
+        plugins: [
+            // Ensure that only requests that result in a 200 status are cached
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    }),
+);
 
-
-// import { registerRoute } from 'workbox-routing';
-// import {
-//     NetworkFirst,
-//     StaleWhileRevalidate,
-//     CacheFirst,
-// } from 'workbox-strategies';
-//
-// // Used for filtering matches based on status code, header, or both
-// import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-// // Used to limit entries in cache, remove entries after a certain period of time
-// import { ExpirationPlugin } from 'workbox-expiration';
-//
-// // Cache page navigations (html) with a Network First strategy
-// registerRoute(
-//     // Check to see if the request is a navigation to a new page
-//     ({ request }) => request.mode === 'navigate',
-//     // Use a Network First caching strategy
-//     new NetworkFirst({
-//         // Put all cached files in a cache named 'pages'
-//         cacheName: 'pages',
-//         plugins: [
-//             // Ensure that only requests that result in a 200 status are cached
-//             new CacheableResponsePlugin({
-//                 statuses: [200],
-//             }),
-//         ],
-//     }),
-// );
-//
-// // Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
-// registerRoute(
-//     // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
-//     ({ request }) =>
-//         request.destination === 'style' ||
-//         request.destination === 'script' ||
-//         request.destination === 'worker',
-//     // Use a Stale While Revalidate caching strategy
-//     new StaleWhileRevalidate({
-//         // Put all cached files in a cache named 'assets'
-//         cacheName: 'assets',
-//         plugins: [
-//             // Ensure that only requests that result in a 200 status are cached
-//             new CacheableResponsePlugin({
-//                 statuses: [200],
-//             }),
-//         ],
-//     }),
-// );
-//
-// // Cache images with a Cache First strategy
-// registerRoute(
-//     // Check to see if the request's destination is style for an image
-//     ({ request }) => request.destination === 'image',
-//     // Use a Cache First caching strategy
-//     new CacheFirst({
-//         // Put all cached files in a cache named 'images'
-//         cacheName: 'images',
-//         plugins: [
-//             // Ensure that only requests that result in a 200 status are cached
-//             new CacheableResponsePlugin({
-//                 statuses: [200],
-//             }),
-//             // Don't cache more than 50 items, and expire them after 30 days
-//             new ExpirationPlugin({
-//                 maxEntries: 50,
-//                 maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-//             }),
-//         ],
-//     }),
-// );
+// Cache images with a Cache First strategy
+workbox.routing.registerRoute(
+    // Check to see if the request's destination is style for an image
+    ({ request }) => request.destination === 'image',
+    // Use a Cache First caching strategy
+    new workbox.strategies.CacheFirst({
+        // Put all cached files in a cache named 'images'
+        cacheName: 'images',
+        plugins: [
+            // Ensure that only requests that result in a 200 status are cached
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+            // Don't cache more than 50 items, and expire them after 30 days
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+            }),
+        ],
+    }),
+);
