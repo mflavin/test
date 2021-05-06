1f81a2251ab28e20c9833509d3f64a2e22944be1
mflavin <mflavin@hawk.iit.edu>
2021-05-06 10:34:25 -0500

    Added... Thu, May  6, 2021 10:34:25 AM

--- src/sw.js
+++ src/sw.js
@@ -1,38 +1,14 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-undef */
+import {
+	registerRoute
+} from 'workbox-routing';
+import {
+	StaleWhileRevalidate
+} from 'workbox-strategies';
 
-// default page handler for offline usage,
-// where the browser does not how to handle deep links
-// it's a SPA, so each path that is a navigation should default to index.html
-workbox.routing.registerRoute(
-  ({
-    event
-  }) => event.request.mode === 'navigate',
-  async () => {
-    // const defaultBase = globalRoute || '/';
-    const defaultBase = '/test/';
-    return caches
-      .match(workbox.precaching.getCacheKeyForURL(defaultBase))
-      .then(response => {
-        console.log('fetch(defaultBase), ', fetch(defaultBase));
-        return response || fetch(defaultBase);
-      })
-      .catch(err => {
-        return fetch(defaultBase);
-      });
-  }
+registerRoute(
+	({
+		request
+	}) => request.destination === 'script' ||
+	request.destination === 'style',
+	new StaleWhileRevalidate()
 );
-
-workbox.core.setCacheNameDetails({ prefix: 'amplify-datastore' });
-// workbox.core.skipWaiting();
-// workbox.core.clientsClaim();
-
-const cacheFiles = [
-  {
-    revision: '032520210139',
-    url: 'sw.js',
-  },
-];
-
-self.__precacheManifest = cacheFiles.concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
